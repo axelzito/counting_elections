87ca564ced6d0cd475b3845d4bdd6c37011720c7
Axel Miguez <axel.ataides@gmail.com>
2022-10-02 20:31:36 -0300

    auto update

--- main.py
+++ main.py
@@ -1,34 +1,49 @@
-import requests
 import json
+import os
+import time
+from datetime import datetime
 import pandas as pd
+import requests
 
-data = requests.get('https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json')
-json_data = json.loads(data.content)
+flag = 0
+while True:
+    if flag != 0:
+        time.sleep(30)
+        os.system('clear')
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print('Atualizado em: ', current_time)
+    data = requests.get(
+        'https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json')
+    json_data = json.loads(data.content)
 
-candidate = []
-party = []
-votes = []
-votes_int = []
-percentage = []
-percentage_int = []
+    candidate = []
+    party = []
+    votes = []
+    votes_int = []
+    percentage = []
+    percentage_int = []
 
-for percentual in json_data['cand']:
+    for percentual in json_data['cand']:
 
-    if percentual['seq'] == '1' or percentual['seq'] == '2' or percentual['seq'] == '3' or percentual['seq'] == '4':
-        candidate.append(percentual['nm'])
-        votes.append("{:,}".format(int(percentual['vap'])))
-        votes_int.append(int(percentual['vap']))
-        percentage.append(percentual['pvap'] + '%')
-        percentage_int.append(float(percentual['pvap'].replace(',', '').replace('%', ''))/100)
+        if percentual['seq'] == '1' or percentual['seq'] == '2' or percentual['seq'] == '3' or percentual['seq'] == '4':
+            candidate.append(percentual['nm'])
+            votes.append("{:,}".format(int(percentual['vap'])))
+            votes_int.append(int(percentual['vap']))
+            percentage.append(percentual['pvap'] + '%')
+            percentage_int.append(float(percentual['pvap'].replace(',', '').replace('%', '')) / 100)
 
-valid_votes = "{:,}".format(int(json_data['vv']))
-total_percentage = json_data['psi'] + '%'
+    valid_votes = "{:,}".format(int(json_data['vv']))
+    total_percentage = json_data['psi'] + '%'
 
-output_results = pd.DataFrame(list(zip(candidate, votes, percentage)),
-                              columns=['Candidato', 'Votos', 'Porcentagem'])
+    output_results = pd.DataFrame(list(zip(candidate, votes, percentage)),
+                                  columns=['Candidato', 'Votos', 'Porcentagem'])
 
-print('\nVotos validos apurados: ', valid_votes,
-      '\nPorcentagem de urnas apuradas: ', total_percentage, '\n')
-print('Diferença de votos entre Lula e Bolsonaro: ', ("{:,}".format(votes_int[0] - votes_int[1])))
-print('Diferença de porcentagem entre Lula e Bolsonaro: ', ("{0:.2f}".format(percentage_int[0] - percentage_int[1])) + '\n')
-print(output_results)
+    print('\nVotos validos apurados: ', valid_votes,
+          '\nPorcentagem de urnas apuradas: ', total_percentage, '\n')
+    print('Diferença de votos entre Lula e Bolsonaro: ', ("{:,}".format(votes_int[0] - votes_int[1])))
+    print('Diferença de porcentagem entre Lula e Bolsonaro: ',
+          ("{0:.2f}".format(percentage_int[0] - percentage_int[1])) + '\n')
+    print(output_results)
+    print('\nPor favor espere 30 segundos ate atualizar!')
+    flag = 1
