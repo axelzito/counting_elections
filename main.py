ad11dc5098cea1b5596676977b05a7998087db4f
Axel Miguez <axel.ataides@gmail.com>
2022-10-02 20:14:18 -0300

    show difference between the first two candidates

--- main.py
+++ main.py
@@ -8,15 +8,18 @@ json_data = json.loads(data.content)
 candidate = []
 party = []
 votes = []
+votes_int = []
 percentage = []
-total = []
+percentage_int = []
 
 for percentual in json_data['cand']:
 
     if percentual['seq'] == '1' or percentual['seq'] == '2' or percentual['seq'] == '3' or percentual['seq'] == '4':
         candidate.append(percentual['nm'])
         votes.append("{:,}".format(int(percentual['vap'])))
+        votes_int.append(int(percentual['vap']))
         percentage.append(percentual['pvap'] + '%')
+        percentage_int.append(float(percentual['pvap'].replace(',', '').replace('%', ''))/100)
 
 valid_votes = "{:,}".format(int(json_data['vv']))
 total_percentage = json_data['psi'] + '%'
@@ -24,5 +27,8 @@ total_percentage = json_data['psi'] + '%'
 output_results = pd.DataFrame(list(zip(candidate, votes, percentage)),
                               columns=['Candidato', 'Votos', 'Porcentagem'])
 
-print('\nVotos validos apurados: ', valid_votes, '\nPorcentagem de urnas apuradas: ', total_percentage, '\n')
+print('\nVotos validos apurados: ', valid_votes,
+      '\nPorcentagem de urnas apuradas: ', total_percentage, '\n')
+print('Diferença  de votos entre Lula e Bolsonaro: ', ("{:,}".format(votes_int[0] - votes_int[1])))
+print('Diferença  de votos entre Lula e Bolsonaro: ', ("{0:.2f}".format(percentage_int[0] - percentage_int[1])) + '\n')
 print(output_results)
