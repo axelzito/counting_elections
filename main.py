6bc722f2a6f7af96b7337711d402bbba7066f0e3
Axel Miguez <axel.ataides@gmail.com>
2022-10-02 19:04:05 -0300

    improve date presentation

--- main.py
+++ main.py
@@ -15,13 +15,14 @@ for percentual in json_data['cand']:
 
     if percentual['seq'] == '1' or percentual['seq'] == '2' or percentual['seq'] == '3' or percentual['seq'] == '4':
         candidate.append(percentual['nm'])
-        votes.append(percentual['vap'])
-        percentage.append(percentual['pvap'])
+        votes.append("{:,}".format(int(percentual['vap'])))
+        percentage.append(percentual['pvap'] + '%')
 
-valid_votes = json_data['vv']
-total_percentage = json_data['psi']
+valid_votes = "{:,}".format(int(json_data['vv']))
+total_percentage = json_data['psi'] + '%'
 
 output_results = pd.DataFrame(list(zip(candidate, votes, percentage)),
                               columns=['Candidato', 'Votos', 'Porcentagem'])
-print('\nVotos validos apurados: ', valid_votes, '\nPorcentagem de urnas apuradas: ', total_percentage, '%\n')
+
+print('\nVotos validos apurados: ', valid_votes, '\nPorcentagem de urnas apuradas: ', total_percentage, '\n')
 print(output_results)
